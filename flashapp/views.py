751ec9c7ed4b447aa424f04e237c866116266c44
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-20 22:50:59 +0300

    Add templates all_cards

--- flashapp/views.py
+++ flashapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from .models import FlashCard
+import random
+import csv
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+    if request.user.is_authenticated:
+        all_cards = FlashCard.objects.filter(creator=request.user.id)
+    else:
+        all_cards = FlashCard.objects.all()
+    #cards = sorted(all_cards.order_by('front'), key=lambda x: random.random())
+  
+    return render(request, 'home.html')
