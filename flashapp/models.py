2fdb1a5bb0114d719fe9389c10db0579850a6d96
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-20 21:51:57 +0300

    Add class flashcard

--- flashapp/models.py
+++ flashapp/models.py
@@ -1,6 +1,6 @@
-
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 # FlashCard Model
@@ -9,8 +9,7 @@ class FlashCard(models.Model):
 	front = models.TextField()
 	back = models.TextField()
 	creator = models.ForeignKey(User, on_delete=models.CASCADE)
-	likes = models.IntegerField(default=0)
-	known = models.IntegerField(default=0)
+	timestamp = models.DateTimeField(default=timezone.now())
 
 	def __str__(self):
 		return self.front
