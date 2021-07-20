1e710a4952d4947d0f46a84287edc19611f6343c
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-20 21:46:40 +0300

    Add urls file

--- flashapp/models.py
+++ flashapp/models.py
@@ -1,3 +1,16 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# FlashCard Model
+class FlashCard(models.Model):
+	category = models.CharField(max_length=50)
+	front = models.TextField()
+	back = models.TextField()
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+	likes = models.IntegerField(default=0)
+	known = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.front
