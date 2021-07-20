693b18012d7d5dc7bc6954d55bd0b648fba92c3a
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-20 23:40:33 +0300

    add redister view

--- flashapp/views.py
+++ flashapp/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
+def registerPage(request):
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('loginpage') 
+        name = form.cleaned_data.get("username")
+        messages.success(request, 'Account was created for' , name)
+    context = {'form':form, 'profile':profile}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Incorrect Username or Password')
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+def logoutpage(request):
+    logout(request)
+    return redirect('loginpage')
