2b4878f7ae2c7f41bc6dd4493eb79f77a16c908c
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-20 21:56:28 +0300

    register database flashcard in setting

--- flashproject/settings.py
+++ flashproject/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'flashproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flashcard',   
+        'USER': 'moringa',      
+        'PASSWORD':'Ammoh',
     }
 }
 
