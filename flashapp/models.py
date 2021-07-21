bdc9aab3bd5a152058d2831596f72bd867797844
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-21 10:39:20 +0300

    add profile model

--- flashapp/models.py
+++ flashapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True, default='No bio')
+    profile= models.ImageField(upload_to = 'images/',default='SOME IMAGE')
+
+    def __str__(self):
+        return self.user.username
+    
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls,name):
+        return cls.objects.filter(user__username__icontains=name).all()
--- flashapp/views.py
+++ flashapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from flashapp.forms import CreateUserForm
 
 
 # Create your views here.
