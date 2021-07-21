f7fe1f18367e7319ac27d105204c2db4197398c4
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-21 23:35:04 +0300

    Add serializer

--- flashapp/serializer.py
+++ flashapp/serializer.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-<<<<<<< HEAD
 from .models import *
 
 
@@ -7,11 +6,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('profile_pic', 'bio', 'contact', 'user' )
-=======
 from .models import deck
 
 class DeckSerializer(serializers.ModelSerializer):
     class Meta:
         model = deck
         fields = ('user','name')
->>>>>>> 18de43d0b432db371a13b9a1020d02b5f2bf5000
--- flashapp/views.py
+++ flashapp/views.py
@@ -119,6 +119,18 @@ def updateFlash(response, id):
             return HttpResponseRedirect("/deck-%d" %card.deck.id)
     return render(response, "flashupdate.html", context)
 
+class ProfileList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+
+    def get(self, request, format=None):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
 class DeckList(APIView):
     def get(self, request, format=None):
         all_decks = deck.objects.all()
